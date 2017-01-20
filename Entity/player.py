61f40ba11648b5febfaf3447006641ac146b6ef6
Taiwo Kareem <taiwo.kareem36@gmail.com>
2017-01-20 16:21:55 +0000

    Update player.py

--- Entity/player.py
+++ Entity/player.py
@@ -1,6 +1,7 @@
 class Player:
-	def __init__(self, name, skill, position):
+	def __init__(self, name, skill, position, fitness):
 		self.name = name
 		self.skill = skill
 		self.position = position
+		self.fitness = fitness
 		

2dd305aa206e307c309180e20e8c6ec6f98e14b9
Taiwo Kareem <taiwo.kareem36@gmail.com>
2017-01-20 16:21:19 +0000

    Create skill.py

---
+++ Entity/skill.py
@@ -0,0 +1,9 @@
+class Skill:
+	def __init__(self, save, defence, midfield, attack, speed):
+		self.save = save
+		self.defence = defence
+		self.midfield = midfield
+		self.attack = attack
+		self.speed = speed
+		
+	

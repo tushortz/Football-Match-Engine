36c41e7e41ff81a81fc55144e99053972d373636
Taiwo Kareem <taiwo.kareem36@gmail.com>
2017-01-20 16:16:03 +0000

    Update player.py

--- Entity/player.py
+++ Entity/player.py
@@ -1,3 +1,6 @@
-# Football Match Engine
-
-Sample test to write a football match engine
+class Player:
+	def __init__(self, name, skill, position):
+		self.name = name
+		self.skill = skill
+		self.position = position
+		

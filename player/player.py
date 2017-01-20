28f49a9e532b4aa332f47c42a4b6146d4000c35e
Taiwo Kareem <taiwo.kareem36@gmail.com>
2017-01-20 17:05:46 +0000

    Update player.py

--- player/player.py
+++ player/player.py
@@ -1,7 +1,16 @@
+import random
+
 class Player:
 	def __init__(self, name, skill, position, fitness):
 		self.name = name
 		self.skill = skill
 		self.position = position
+		self.luck = random.randint(0, 100) * 0.01
 		self.fitness = fitness
 		
+	def shoot(self, goal_keeper):
+		if ((self.skill.attack + self.skill.speed + self.fitness) * self.luck) and 
+			(goal_keeper.skill.save + goal_keeper.skill.speed + goal_keeper.fitness) * goal_keeper.luck):
+				return 1
+		else:
+			return 0			
